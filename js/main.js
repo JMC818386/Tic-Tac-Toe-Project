d8d4bd9fd77410d47ad5720f1c1a3fa6a486ea25
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-02-23 00:41:35 +0000

    Removed inner text from cells Created restart button

--- js/main.js
+++ js/main.js
@@ -12,63 +12,63 @@ gameBoard.className = "col-12";
 //cell0 variable (top left) creates empty div
 let cell0 = document.createElement("h1");
 gameBoard.appendChild(cell0);
-cell0.innerText = "X"
+cell0.innerText = ""
 cell0.className = "cell col-4 d-flex justify-content-center align-items-center border border-3";
 
 //cell1 variable (top center)
 let cell1 = document.createElement("h1");
 gameBoard.appendChild(cell1);
-cell1.innerText = "X";
+cell1.innerText = "";
 cell1.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell2 variable (top right)
 let cell2 = document.createElement("h1");
 gameBoard.appendChild(cell2);
-cell2.innerText = "X"
+cell2.innerText = ""
 cell2.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell3 variable (middle left)
 let cell3 = document.createElement("h1");
 gameBoard.appendChild(cell3);
-cell3.innerText = "X"
+cell3.innerText = ""
 cell3.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell4 variable (middle center)
 let cell4 = document.createElement("h1");
 gameBoard.appendChild(cell4);
-cell4.innerText = "X"
+cell4.innerText = ""
 cell4.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell5 variable (middle right)
 let cell5 = document.createElement("h1");
 gameBoard.appendChild(cell5);
-cell5.innerText = "X"
+cell5.innerText = ""
 cell5.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell6 variable (bottom left)
 let cell6 = document.createElement("h1");
 gameBoard.appendChild(cell6);
-cell6.innerText = "X"
+cell6.innerText = ""
 cell6.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell7 variable (bottom center)
 let cell7 = document.createElement("h1");
 gameBoard.appendChild(cell7);
-cell7.innerText = "X"
+cell7.innerText = ""
 cell7.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //cell8 variable (bottom right)
 let cell8 = document.createElement("h1");
 gameBoard.appendChild(cell8);
-cell8.innerText = "X"
+cell8.innerText = ""
 cell8.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
 
 //-------------------------------------------------------------------//
 
-let resetBtn = document.createElement("h1");
-gameBoard.appendChild(cell8);
-cell8.innerText = "X"
-cell8.className = "cell col-4 d-flex justify-content-center align-items-center border border-4";
+let restartBtn = document.createElement("button");
+gameBoard.appendChild(restartBtn);
+restartBtn.innerText = "Restart Game"
+restartBtn.className = "col-2 d-flex justify-content-center align-items-center border border-4";
 
 
 
